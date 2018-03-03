2b38bfd50ce36083d00fe128bcac6ffbce45ef25
Alexander Mikhaylov <A.Mikhaylov.95@gmail.com>
2018-03-03 15:39:10 +0300

    Изменить структуру
    * Добавлен create-react-app

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/data', (req, res) => {
+    res.json({test1: 1, test2: 2, test3: 3});
+});
+
+require('./server/routes')(app);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Server is listening on ${port}`);
