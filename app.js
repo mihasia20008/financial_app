1c2929de28e227abf9eafb373a76240fc2e5d1eb
Alexander Mikhaylov <A.Mikhaylov.95@gmail.com>
2018-03-17 13:42:02 +0300

    Изменить структуру сервера проектаё

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
 
 const app = express();
 
 app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
