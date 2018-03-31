1548a10bbf171e645f26389d1446a4a0f0a0f551
Alexander Mikhaylov <A.Mikhaylov.95@gmail.com>
2018-03-31 17:30:28 +0300

    Создать пути для взаимодействия с сущностями

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ require('./server/routes')(app);
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 const usersController = require('../controllers').users;
 const billsController = require('../controllers').bills;
+const operationsController = require('../controllers').operations;
 const categoriesController = require('../controllers').categories;
 
 module.exports = (app) => {
@@ -7,19 +8,34 @@ module.exports = (app) => {
     message: 'Welcome to the API!'
   }));
 
-  app.post('/api/categories/new', categoriesController.create);
-  app.get('/api/categories/:id', categoriesController.show);
-  app.delete('/api/categories/:id', categoriesController.delete);
-  app.put('/api/categories/:id', categoriesController.update);
-
-  app.post('/api/bills/new', billsController.create);
-  app.get('/api/bills/:id', billsController.show);
-  app.delete('/api/bills/:id', billsController.delete);
-  app.put('/api/bills/:id', billsController.update);
-
-  app.post('/api/users/new', usersController.create);
+  app.post('/api/signup', usersController.create);
   app.post('/api/login', usersController.auth);
   app.get('/api/users/:id', usersController.show);  
   // app.delete('/api/users/:id', usersController.delete);
   // app.put('/api/users/:id', usersController.update);
+
+  app.post('/api/bill/update', (req, res) => {
+    const { id, value } = req.body;
+    billsController.changeBalance(id, +value)
+      .then(bill => res.status(202).send(bill))
+      .catch(err => res.status(400).send(err.toString()));
+  });
+
+  app.get('/api/bill', billsController.showAll);
+  app.post('/api/bill', billsController.create);
+  app.put('/api/bill', billsController.update);
+  app.delete('/api/bill', billsController.delete);  
+  app.get('/api/bill/:id', billsController.showOne);
+
+  app.get('/api/operation', operationsController.showAll);
+  app.post('/api/operation', operationsController.create);
+  // app.put('/api/operation', operationsController.update);
+  app.delete('/api/operation', operationsController.delete);  
+  app.get('/api/operation/:id', operationsController.showOne);
+
+  app.get('/api/category', categoriesController.showAll);
+  app.post('/api/category', categoriesController.create);
+  app.put('/api/category', categoriesController.update);
+  app.delete('/api/category', categoriesController.delete);  
+  app.get('/api/category/:id', categoriesController.showOne);
 };
