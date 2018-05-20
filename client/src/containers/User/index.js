9499bb0603c971930b83fa3741090770ca9aa300
Alexander Mikhaylov <A.Mikhaylov.95@gmail.com>
2018-05-20 18:11:07 +0300

    Реализация клиентского роутинга

---
+++ client/src/containers/User/index.js
@@ -0,0 +1,14 @@
+import SignInPage from './SignInPage';
+import SignUpPage from './SignUpPage';
+import ForgotPage from './ForgotPage';
+import RestorePasswordPage from './RestorePasswordPage';
+import VerifyPage from './VerifyPage';
+import NeedConfirmPage from './NeedConfirmPage';
+
+export { 
+    SignInPage, 
+    SignUpPage, 
+    ForgotPage, 
+    RestorePasswordPage, 
+    VerifyPage, 
+    NeedConfirmPage };
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,2 +1,28 @@
-export * from './main';
-// export * from './interaction';
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import Container from '../containers/Container';
+import NotFoundPage from '../containers/NotFoundPage';
+
+import { 
+    SignInPage, 
+    SignUpPage, 
+    ForgotPage, 
+    RestorePasswordPage, 
+    VerifyPage, 
+    NeedConfirmPage } from '../containers/User';
+
+import ActionsPage from '../containers/ActionsPage';
+
+export default (
+    <Switch>
+        <Container exact path="/" component={SignInPage} />
+        <Container path="/signup" component={SignUpPage} />
+        <Container path="/forgotpassword" component={ForgotPage} />
+        <Container path="/restorepassword" component={RestorePasswordPage} />
+        <Container path="/verify" component={VerifyPage} />
+        <Container path="/needconfirm" component={NeedConfirmPage} />
+        <Container path="/actions" privateRoute component={ActionsPage} />
+        <Container component={NotFoundPage} />
+    </Switch>
+);
